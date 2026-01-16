aa3927ef9b9a470280a2a26f1421df7ec5bae5c1
Aur1sh <pradhansaurisharup277@gmail.com>
2026-01-16 14:59:45 +0000

    codeforces problem B

---
+++ codeforcesB.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+    int a;
+    long long int   b;
+    char ch ;
+    float flo;
+    double d;
+    cin >> a >> b >> ch >> flo >> d ;
+    cout << a << endl;
+    cout << b << endl;
+    cout << ch << endl;
+    cout << flo << endl;
+    cout << d << endl;
+
+    return 0;
+}
