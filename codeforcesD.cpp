6a181a1373b8d776c71c6090c2beda180fdd873f
Aur1sh <pradhansaurisharup277@gmail.com>
2026-01-16 15:07:57 +0000

    codeforces problem D

---
+++ codeforcesD.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    long long int A , B , C ,D;
+    cin >> A >> B >> C >> D;
+
+    long long int X = (A* B) - (C*D);
+    cout << "Difference = " << X;
+
+    return 0;
+}
