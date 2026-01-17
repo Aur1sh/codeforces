23cdc6e23b2d46adf01a6bc90ae3227360ae20a2
Aur1sh <pradhansaurisharup277@gmail.com>
2026-01-17 16:09:00 +0000

    CodeforcesI

---
+++ codeforcesI.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    long long int A, B;
+    cin >> A >> B;
+    if(A>B || A == B){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+    return 0;
+}
