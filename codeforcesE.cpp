f08937bc834757edecc22867ed676924b39bdde9
Aur1sh <pradhansaurisharup277@gmail.com>
2026-01-17 16:03:44 +0000

    CodeforcesE

---
+++ codeforcesE.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main (){
+        double pi = 3.141592653;
+        double R;
+        cin >> R;
+        double Area = pi * R * R;
+        cout << fixed << setprecision(9) << Area << endl;
+        
+
+    return 0;
+}
